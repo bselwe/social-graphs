4ac7c6102310da59fb5547b842d2f577807fe001
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2020-12-06 20:59:06 +0100

    Added svg to TF-TR (not working atm)

--- src/components/TFTR/index.tsx
+++ src/components/TFTR/index.tsx
@@ -1,32 +1,36 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
 
-import pop from "../../media/pop.png";
+//import pop from "../../media/pop.svg";
+import pop from "../../media/pop.svg";
 import rap from "../../media/rap.png";
+import r_and_b from "../../media/pop.svg"
+import rock from "../../media/pop.svg"
+import jazz from "../../media/pop.svg"
 
 enum ComparedGenres {
-  PopRap,
-  Second,
-  Third,
+  PopVsRap,
+  RhythmAndBluesVsRock,
+  PopVsJazz,
 }
 
 const TF_TR: React.FC = () => {
   const [comparedGenres, setComparedGenres] = useState<ComparedGenres>(
-    ComparedGenres.PopRap
+    ComparedGenres.PopVsRap
   );
 
   return (
     <div className={styles.container}>
       <p>Please select a pair of genres</p>
 
-      <button onClick={() => setComparedGenres(ComparedGenres.PopRap)}>
+      <button onClick={() => setComparedGenres(ComparedGenres.PopVsRap)}>
         Pop vs Rap
       </button>
-      <button onClick={() => setComparedGenres(ComparedGenres.Second)}>
-        Second
+      <button onClick={() => setComparedGenres(ComparedGenres.RhythmAndBluesVsRock)}>
+        Rhythm and Blues vs Rock
       </button>
-      <button onClick={() => setComparedGenres(ComparedGenres.Third)}>
-        Third
+      <button onClick={() => setComparedGenres(ComparedGenres.PopVsJazz)}>
+        Pop vs Jazz
       </button>
 
       {comparedGenres !== undefined
@@ -38,11 +42,25 @@ const TF_TR: React.FC = () => {
 
 function displayComparedGenres(genres: ComparedGenres) {
   switch (genres) {
-    case ComparedGenres.PopRap:
+    case ComparedGenres.PopVsRap:
       return (
         <div>
           <img src={pop} alt="pop" />
           <img src={rap} alt="rap" />
+        </div>
+      );
+    case ComparedGenres.RhythmAndBluesVsRock:
+      return (
+        <div>
+          <img src={r_and_b} alt="rhythm and blues" />
+          <img src={rock} alt="rock" />
+        </div>
+      );
+    case ComparedGenres.PopVsJazz:
+      return (
+        <div>
+          <img src={pop} alt="pop" />
+          <img src={jazz} alt="jazz" />
         </div>
       );
     default:
