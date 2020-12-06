37e9daf8a87c4d655044f2c88b5c2677507718f5
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2020-12-06 21:51:13 +0100

    Prepare artists section

--- src/components/Artists/index.tsx
+++ src/components/Artists/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import styles from "./styles.module.css";
 
 import popularities from "./popularities.svg";
+import genres from "./genres.svg";
+import degreeDistribution from "./degree_distribution.svg";
+import degreeDistributionLogLog from "./degree_distribution_log_log.svg";
 
 const Artists: React.FC = () => {
   return (
     <div className={styles.container}>
       <p>Introduce artists data set</p>
       <img src={popularities} width={800} alt={"popularities"} />
+      <img src={genres} width={800} alt={"genres"} />
+      <img src={degreeDistribution} width={1200} alt={"degree distribution"} />
+      <img
+        src={degreeDistributionLogLog}
+        width={800}
+        alt={"degree distribution (log log)"}
+      />
     </div>
   );
 };
